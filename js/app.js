926515368498e8fd81440bf6b17d0cf246753f7d
Dale Payne <dalebird88@gmail.com>
2016-06-27 10:02:14 -0600

    skeleton

---
+++ js/app.js
@@ -0,0 +1,14 @@
+angular.module("app",['ui.router']);
+
+angular.module("app").config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+    .state('home', {
+            url: '/',
+            templateUrl: '../routes/home.html'
+        })
+
+  });
---
+++ js/mainCtrl.js
@@ -0,0 +1,2 @@
+angular.module("app")
+.controller("mainCtrl", function($scope, mainService){});
